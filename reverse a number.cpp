77db0a1ed017e96415fecf56a798777817278911
moksha27 <112235188+moksha27@users.noreply.github.com>
2022-10-31 09:19:58 +0530

    Add files via upload

---
+++ reverse a number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,t1=0,t2=1,nextterm=0;
+	cout<<"enter the number of terms:";
+	cin>>n;
+	cout<<"fibonacci series:";
+	for(int i=1;i<=n;++i){
+		//prints the first two terms.
+		if(i==1){
+			cout<<t1<<",";
+			continue;
+		}
+		if(i==2){
+			cout<<t2<<",";
+			continue;
+		}
+		nextterm=t1+t2;
+		t1=t2;
+		t2=nextterm;
+		cout<<nextterm<<",";
+	}
+	return 0;
+}
